d0d151dc5e25d8f9caef65811b81bb82f1ff2a7d
John Haugeland <stonecypher@gmail.com>
2014-06-29 22:04:20 -0700

    and now a node module

--- src/lib/deepak_node.js
+++ src/lib/deepak_node.js
@@ -3,17 +3,24 @@
 
 'use strict';
 
-
-
-
-
 var fs     = require('fs');
 var path   = require('path');
-var libdir = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
-var source = (process.argv.length > 2)? process.argv[2] : path.join(libdir, '/default_deepak_data.js');
 
-if (fs.existsSync(source)) {
-	console.log('Found it.');
-} else {
-	console.log('\nFatal error: requested source datafile does not exist.\n  "' + source + '"');
+
+
+
+
+function DeepakGenerator() {
+
+    var libdir = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
+    var source = (process.argv.length > 2)? process.argv[2] : path.join(libdir, '/default_deepak_data.js');
+
+    if (fs.existsSync(source)) {
+        console.log('Found it.');
+    } else {
+        console.log('\nFatal error: requested source datafile does not exist.\n  "' + source + '"');
+    }
+
 }
+
+exports.choprify = DeepakGenerator;
