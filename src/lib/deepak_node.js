ef7e23a71a40a72ad0a6090175ce5982b3a65b83
John Haugeland <stonecypher@gmail.com>
2014-06-29 22:24:30 -0700

    closer

--- src/lib/deepak_node.js
+++ src/lib/deepak_node.js
@@ -3,8 +3,8 @@
 
 'use strict';
 
-var fs     = require('fs');
-var path   = require('path');
+var fs   = require('fs');
+var path = require('path');
 
 
 
@@ -12,14 +12,25 @@ var path   = require('path');
 
 function DeepakGenerator() {
 
+    function randFrom(X) {
+        return X[ Math.floor(Math.random() * X.length) ];
+    }
+
+    function makeDC() {
+        return randFrom(Data.starts) + randFrom(Data.middles) + randFrom(Data.qualifiers) + randFrom(Data.finishes);
+    }
+
     var libdir = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
-    var source = (process.argv.length > 2)? process.argv[2] : path.join(libdir, '/default_deepak_data.js');
+    var source = (process.argv.length > 2)? process.argv[2] : path.join(libdir, '/default_deepak_data.json');
 
     if (fs.existsSync(source)) {
         console.log('Found it.');
+        var Data = require(source);
     } else {
         console.log('\nFatal error: requested source datafile does not exist.\n  "' + source + '"');
     }
+
+
 
 }
 
