6568dc4b4fd22f5c208dc0d2e2e7cc62e5abf908
John Haugeland <stonecypher@gmail.com>
2014-06-29 22:31:40 -0700

    pattern is now generic :)

--- src/lib/deepak_node.js
+++ src/lib/deepak_node.js
@@ -12,20 +12,28 @@ var path = require('path');
 
 function DeepakGenerator() {
 
-    function randFrom(X) {
-        return X[ Math.floor(Math.random() * X.length) ];
-    }
 
-    function makeDC() {
-        return randFrom(Data.starts) + randFrom(Data.middles) + randFrom(Data.qualifiers) + randFrom(Data.finishes);
-    }
 
     var libdir = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
     var source = (process.argv.length > 2)? process.argv[2] : path.join(libdir, '/default_deepak_data.json');
 
+
+
+    function randFrom(X) {
+        return X[ Math.floor(Math.random() * X.length) ];
+    }
+
+    function makeDC(Data) {
+        var out = '';
+        for (var i in Data.pattern) { out += randFrom(Data[Data.pattern[i]]); }
+        return out;
+    }
+
+
+
     if (fs.existsSync(source)) {
-        console.log('Found it.');
         var Data = require(source);
+        console.log(makeDC(Data));
     } else {
         console.log('\nFatal error: requested source datafile does not exist.\n  "' + source + '"');
     }
@@ -33,5 +41,9 @@ function DeepakGenerator() {
 
 
 }
+
+
+
+
 
 exports.choprify = DeepakGenerator;
